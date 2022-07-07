85325ee943a39b77a9e15a976d6d4e8a3aeff250
KingOfFondness <106331933+KingOfFondness@users.noreply.github.com>
2022-07-07 05:42:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,25 +12,25 @@ def is_enabled(value, default):
 
 
 # Creator
-CREATOR_NAME = os.environ.get("CREATOR_NAME", "𝙼𝚄𝙷𝙰𝙼𝙼𝙴𝙳")
-CREATOR_USERNAME = os.environ.get("CREATOR_USERNAME", "PR0FESS0_99")
+CREATOR_NAME = os.environ.get("CREATOR_NAME", "KingOfFondness")
+CREATOR_USERNAME = os.environ.get("CREATOR_USERNAME", "KingOfFondness")
 
 # Account
-API_HASH = os.environ.get("API_HASH", "5b1d0992294a67cb54512a4fafeb0c88")
-API_ID = os.environ.get("API_ID", "6170803")
+API_HASH = os.environ.get("API_HASH", "da59e4e56ec4fe35af603bd30208ecc5")
+API_ID = os.environ.get("API_ID", "12635510")
 # About Bot
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "1905228806:AAH2iahJcg5J6bqpcW11jB9KajwHbIjaslGTY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5458262263:AAE-nncwSdmSLJ2HwU0E3qJKa8Zb69n5iAE")
 PICS = os.environ.get("PICS", "https://telegra.ph/file/034d53b5ed1d920ecab8b.jpg")
 # Database
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "LuciferMoringstar-Robot")
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://{Username}:{Passs}@cluster0.{clusterID}.mongodb.net/myFirstDatabase?retryWrites=true{iD}=majority")
 # Chats & Users
-ADMINS = os.environ.get("ADMINS", "2028425293 1637186875")
-SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Mo_Tech_Group")
-AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "-1001685151224")
-CHANNELS = [int(ch) if search.search(ch) else ch for ch in os.environ.get("CHANNELS", "-1001784382279").split()]
-LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001590063851")
-GET_FILECHANNEL = os.environ.get("GET_FILECHANNEL", "-1001570208190")
+ADMINS = os.environ.get("ADMINS", "1130215726 ")
+SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "fondness MoviesHD")
+AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "--1001703747432")
+CHANNELS = [int(ch) if search.search(ch) else ch for ch in os.environ.get("CHANNELS", "--1001293479644").split()]
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001703747432")
+GET_FILECHANNEL = os.environ.get("GET_FILECHANNEL", "-1001739141872")
 FILTER_DEL_SECOND = int(os.environ.get("FILTER_DEL_SECOND", "600"))
 
 # AutoFilter
